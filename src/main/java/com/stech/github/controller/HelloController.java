82b04697bc81c5f09643712c5ea9e9164e541fdf
GineerInGit <sathishkumargineer@gmail.com>
2026-06-11 10:36:25 +0530

    code

---
+++ src/main/java/com/stech/github/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.stech.github.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/github")
+public class HelloController {
+
+	@RequestMapping(method = RequestMethod.GET,value = "/welcome")
+	public String welcomeMethod() {
+		return "<h1>Hello World!</h1>";
+	}
+	
+}
